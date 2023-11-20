175e9789cdc7b4a5cacc9d5a0fbc1888c31406fb
Russell <flahertypro1@outlook.com>
2023-11-20 21:27:23 +0000

    added efcore to project

--- Startup.cs
+++ Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Entity.Framework.Core;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
